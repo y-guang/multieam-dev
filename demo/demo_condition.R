f0da5209d4f3ec8feb3509288f8f7df8f62c055a
spike <guang.spike.yang@gmail.com>
2025-09-18 21:32:19 +1000

    fix: add condition loop to test speed up

--- demo/demo_condition.R
+++ demo/demo_condition.R
@@ -3,7 +3,7 @@ n_items <- 10
 between_trial_formulas <- list(
   n_items ~ 10,
   A ~ 10,
-  V_0 ~ distributional::dist_uniform(0.2, 5),
+  V_0 ~ distributional::dist_uniform(0.01, 0.02),
   ndt ~ 1
 )
 
@@ -31,3 +31,18 @@ res <- run_condition(
   noise_factory = noise_factory,
   trajectories = FALSE
 )
+
+# test speed
+# results <- replicate(100, run_condition(
+#   condition_setting = list(),
+#   between_trial_formulas = between_trial_formulas,
+#   item_formulas = item_formulas,
+#   n_trials = 100,
+#   n_items = n_items,
+#   max_reached = n_items,
+#   max_t = 100,
+#   dt = 0.01,
+#   noise_mechanism = "add",
+#   noise_factory = noise_factory,
+#   trajectories = FALSE
+# ), simplify = FALSE)
