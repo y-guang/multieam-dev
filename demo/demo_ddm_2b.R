350a685f52dc63da35d1e4157f34da8a7a3d468c
spike <guang.spike.yang@gmail.com>
2025-10-31 17:13:52 +1100

    fix: make ddm 2b demo workable

--- demo/demo_ddm_2b.R
+++ demo/demo_ddm_2b.R
@@ -1,4 +1,6 @@
-# placeholder
+###############
+# model setup #
+###############
 n_items <- 2
 
 prior_formulas <- list()
@@ -18,21 +20,42 @@ noise_factory <- function(context) {
   }
 }
 
-sim_result <- run_simulation(
+####################
+# simulation setup #
+####################
+sim_config <- new_simulation_config(
   prior_formulas = prior_formulas,
   between_trial_formulas = between_trial_formulas,
   item_formulas = item_formulas,
-  n_condition = 2,
-  n_trial_per_condition = 2,
+  n_conditions_per_chunk = NULL, # automatic chunking
+  n_conditions = 500,
+  n_trials_per_condition = 100,
   n_items = n_items,
   max_reached = n_items,
   max_t = 100,
   dt = 0.01,
   noise_mechanism = "add",
   noise_factory = noise_factory,
-  trajectories = FALSE,
   model = "ddm-2b",
-  parallel = FALSE
+  parallel = TRUE,
+  n_cores = NULL, # Will use default: detectCores() - 1
+  rand_seed = NULL # Will use default random seed
 )
+print(sim_config)
 
-flat_result <- flatten_simulation_results(sim_result)
+# output temporary path setup
+temp_output_path <- tempfile("multieam_demo_output")
+# remove if exists
+if (dir.exists(temp_output_path)) {
+  unlink(temp_output_path, recursive = TRUE)
+}
+cat("Temporary output path:\n")
+cat(temp_output_path, "\n")
+
+##################
+# run simulation #
+##################
+sim_output <- run_simulation(
+  config = sim_config,
+  output_dir = temp_output_path
+)
